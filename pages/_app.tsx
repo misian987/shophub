4c1edfc46d7f8b2a5d762d6a57d3295dbdb5d415
misian987 <misian987@gmail.com>
2025-03-02 13:45:47 +0200

    Update GTM implementation with new container ID GTM-WSQ3WMQ6

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -58,6 +58,8 @@ function MyApp({ Component, pageProps }: AppProps) {
     initializeDataLayer();
   }, []);
 
+  const GTM_ID = process.env.NEXT_PUBLIC_GTM_ID;
+
   return (
     <>
       <Head>
@@ -69,25 +71,29 @@ function MyApp({ Component, pageProps }: AppProps) {
       </Head>
 
       {/* Google Tag Manager */}
-      <Script id="google-tag-manager" strategy="afterInteractive">
-        {`
-          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-MHXV9WP');
-        `}
-      </Script>
+      {GTM_ID && (
+        <>
+          <Script id="google-tag-manager" strategy="afterInteractive">
+            {`
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','${GTM_ID}');
+            `}
+          </Script>
 
-      {/* Google Tag Manager (noscript) */}
-      <noscript>
-        <iframe
-          src="https://www.googletagmanager.com/ns.html?id=GTM-MHXV9WP"
-          height="0"
-          width="0"
-          style={{ display: 'none', visibility: 'hidden' }}
-        />
-      </noscript>
+          {/* Google Tag Manager (noscript) */}
+          <noscript>
+            <iframe
+              src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+              height="0"
+              width="0"
+              style={{ display: 'none', visibility: 'hidden' }}
+            />
+          </noscript>
+        </>
+      )}
 
       <ThemeProvider theme={theme}>
         <CssBaseline />
---
+++ tatus
@@ -0,0 +1,9 @@
+[33m6da850d[m[33m ([m[1;36mHEAD[m[33m -> [m[1;32mmain[m[33m, [m[1;31morigin/main[m[33m)[m Add static export configuration and GitHub Actions workflow
+[33m5a07846[m Initial commit: ShopHub eCommerce project
+[33m2efd505[m Fix deployment configuration and add custom 404 page
+[33m1f166d7[m Update page metadata and add favicon
+[33m5a21457[m Update README with deployment status and new features
+[33meec527c[m Update Next.js config and add new dataLayer events
+[33m55ef03b[m Update GitHub Pages deployment workflow
+[33mb3bde03[m Update deployment configuration and add README
+[33m69b64e0[m Initial commit: ShopHub eCommerce website
